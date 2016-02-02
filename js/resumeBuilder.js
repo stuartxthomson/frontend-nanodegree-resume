3b56169435c36ad26a0f625e2f93bda725c69142
Stuart Thomson <stuartt@gmail.com>
2016-02-02 09:38:55 -0700

    Add objects for resume. Add image files

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,72 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var bio = {
+   "name" : "Stuart Thomson",
+   "role" : "Web developer",
+   "contacts" : {
+     "mobile" : "780-999-3199",
+     "email" : "stuartt@gmail.com",
+     "github" : "stuartxthomson",
+     "twitter" : "@stuartxthomson",
+     "location" : "Edmonton, Alberta"
+   },
+   "welcomeMessage" : "Welcome to my wonderful resume!",
+   "skills" : [
+     "writing", "HTML5", "CSS", "Javacript (kinda)"
+   ],
+   "bioPic" : "images/stuart.jpg"
+ }
+
+ var education = {
+   "schools" : [
+     {
+       "name" : "Algonquin College",
+       "city" : "Ottawa, Ontario",
+       "degree" : "Diploma",
+       "majors" : "Journalism",
+       "dates" : 2010,
+       "url" : "http://www.algonquincollege.com"
+     },
+     {
+       "name" : "Algonquin College",
+       "city" : "Ottawa, Ontario",
+       "degree" : "Diploma",
+       "majors" : "Aboriginal Studies",
+       "dates" : 2010,
+       "url" : "http://www.algonquincollege.com"
+     }
+   ]
+ }
+
+ var work = {
+   "jobs" : [
+     {
+       "employer" : "Edmonton Journal",
+       "title" : "Website editor",
+       "dates" : "October, 2011 - Present",
+       "description" : "I've worked on web strategy, ebooks, mini-documentaries, and edited hundreds of stories in that time. I am currently responsible for the Edmonton Journal's website, the main twitter feed and the paper's Facebook page. A memo I wrote about getting the most out of Facebook was quickly adopted by Postmedia as the strategy for the whole chain of newspapers."
+     },
+     {
+       "employer" : "Whitecourt Star",
+       "title" : "Reporter/Editor",
+       "dates" : "September, 2010 - October, 2011",
+       "description" : "Reported on and photographed the communities surrounding Whitecourt and Mayerthorpe."
+     }
+   ]
+ }
+
+ var projects = {
+   "projects" : [
+     {
+       "title" : "Sample Project I",
+       "dates" : "2014",
+       "description" : "Here will go a description of the project.",
+       "images" : [
+         "images/photo1.jpg",
+         "images/photo2.jpeg"
+       ]
+     }
+   ]
+ }
